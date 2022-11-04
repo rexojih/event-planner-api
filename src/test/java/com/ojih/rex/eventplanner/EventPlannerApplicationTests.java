e21a5099d5a80510146fcceb1b301f055b227e66
rex.ojih <rex.ojih@gmail.com>
2022-11-03 21:51:44 -0500

    Remove unused imports

--- src/test/java/com/ojih/rex/eventplanner/EventPlannerApplicationTests.java
+++ src/test/java/com/ojih/rex/eventplanner/EventPlannerApplicationTests.java
@@ -2,9 +2,6 @@ package com.ojih.rex.eventplanner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest
 class EventPlannerApplicationTests {
